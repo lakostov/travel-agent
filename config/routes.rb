36fd0c8ef3c7ef029b80a366d3674c415c8a0f2b
Lachezar Kostov <lachezarkostov@gmail.com>
2018-01-22 13:36:23 +0000

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :payments
+  resources :reservations
+  resources :cruises
+  resources :cabins
+  resources :ships
+  resources :credit_cards
+  resources :addresses
+  resources :customers
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ test/system/cruises_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CruisesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit cruises_url
+  #
+  #   assert_selector "h1", text: "Cruise"
+  # end
+end
